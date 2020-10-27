8b0031605069c0154bce90811fc1d121c86c8395
Yuliya <yuliya.sm7@gmail.com>
2020-10-27 12:19:47 +0300

    add 2 chart

--- udp server nodejs/public/js/main.js
+++ udp server nodejs/public/js/main.js
@@ -25,6 +25,7 @@ function updateChart(chart) {
   });
 }
 function updateChart2(chart) {
+  console.log(ADC);
   chart.data.datasets.forEach(function (dataset) {
     dataset.data.push({
       x: Date.now(),
@@ -38,6 +39,7 @@ let conf = {
   data: {
     datasets: [
       {
+        label: "Ток",
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
         data: [], // empty at the beginning
@@ -71,10 +73,49 @@ let conf = {
   },
 };
 
+let conf2 = {
+  type: "line",
+  data: {
+    datasets: [
+      {
+        label: "Напряжение",
+        borderColor: "rgb(54, 162, 235)",
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
+        data: [], // empty at the beginning
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            duration: 20000,
+            refresh: 1000,
+            delay: 1000,
+            pause: false,
+            ttl: undefined,
+
+            onRefresh: updateChart2,
+          },
+        },
+      ],
+    },
+    plugins: {
+      streaming: {
+        frameRate: 30, // chart is drawn 30 times every second
+      },
+    },
+  },
+};
+
 var ctx = document.getElementById("myChart").getContext("2d");
 var chart = new Chart(ctx, conf);
 var ctx2 = document.getElementById("myChart2").getContext("2d");
-var chart = new Chart(ctx, conf);
+var chart = new Chart(ctx2, conf2);
 
 // window.onload = () => {
 //   "use strict";
